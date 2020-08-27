5098685de4c7b79b6e52c58b13b21dc5d7256e96
DaoDucDev <daoducdev@gmail.com>
2020-08-27 23:48:35 +0700

    Update 27/08

---
+++ CrawlData/DAL/CategoryDAL.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+public class CategoryDAL
+{
+    private string query;
+    private MySqlDataReader reader;
+    private MySqlConnection connection;
+
+    public CategoryDAL()
+    {
+        connection = DbHelper.OpenConnection();
+    }
+
+    public List<Category> GetAllCategory()
+    {
+        if (connection.State == System.Data.ConnectionState.Closed)
+        {
+            connection.Open();
+        }
+
+        query = @"select * from category;";
+        MySqlCommand command = new MySqlCommand(query, connection);
+        reader = command.ExecuteReader();
+
+        List<Category> listCategories = new List<Category>();
+
+        if (reader != null)
+        {
+            listCategories = GetListCategories(command);
+        }
+
+        reader.Close();
+        DbHelper.CloseConnection();
+        return listCategories;
+
+    }
+
+    private List<Category> GetListCategories(MySqlCommand command)
+    {
+        List<Category> listCategories = new List<Category>();
+
+        while (reader.Read())
+        {
+            listCategories.Add(GetCategoryInfo(reader));
+        }
+
+        connection.Close();
+
+        return listCategories;
+    }
+
+    private Category GetCategoryInfo(MySqlDataReader reader)
+    {
+        Category cate = new Category();
+        cate.Id = reader.GetInt32("id");
+        cate.Title = reader.GetString("title");
+        cate.Link = reader.GetString("link");
+        cate.Description = reader.GetString("description");
+
+        return cate;
+    }
+}
---
+++ CrawlData/DAL/DbHelper.cs
@@ -0,0 +1,51 @@
+using MySql.Data.MySqlClient;
+
+public class DbHelper
+{
+    private static MySqlConnection connection;
+
+    public static MySqlConnection GetConnection()
+    {
+        if (connection == null)
+        {
+            connection = new MySqlConnection
+            {
+                ConnectionString = @"server=localhost;user id=root;
+                    port=3306;password=123456;database=girls"
+            };
+        }
+
+        return connection;
+    }
+
+    public static MySqlConnection OpenConnection()
+    {
+        if (connection == null)
+        {
+            GetConnection();
+        }
+
+        connection.Open();
+        return connection;
+    }
+
+    public static void CloseConnection()
+    {
+        if (connection != null)
+        {
+            connection.Close();
+        }
+    }
+
+    public static MySqlDataReader ExecQuery(string query)
+    {
+        MySqlCommand command = new MySqlCommand(query, connection);
+        return command.ExecuteReader();
+    }
+
+    public static int ExecNonQuery(string query)
+    {
+        MySqlCommand command = new MySqlCommand(query, connection);
+        return command.ExecuteNonQuery();
+    }
+}
